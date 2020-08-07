032f7c50ad4d4f2c95af2da2c77761da043cbfe7
jaeho-sim-bespin <jaeho.sim@bespinglobal.com>
2020-08-07 18:50:52 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :message="state.msg" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+  name: 'App',  
   components: {
     HelloWorld
+  },
+  setup() {
+    const state = reactive({
+      msg: 'Hello!'
+    });
+    return {
+      state
+    }
   }
 }
 </script>
